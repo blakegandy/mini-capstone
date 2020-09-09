80f442003919fa15136a7b08d3451f3bdde8c84f
blakegandy <gandyb98@gmail.com>
2020-09-09 14:06:56 -0400

    adds find all product action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,11 @@
 class Api::ProductsController < ApplicationController
   def product_action
-    @products = Product.all
+    @product = Product.find_by(id:1)
     render "product.json.jb"
+  end
+
+  def all_products_action
+    @products = Product.all
+    render "all_products.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/product_path" => "products#product_action"
+    get "/products_path" => "products#all_products_action"
   end
 end
