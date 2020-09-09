85fdc9c6007f227cf001bc34541779822aee69b9
blakegandy <gandyb98@gmail.com>
2020-09-09 13:30:50 -0400

    adds four new products to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.new(name: "banana", price: 0.50, image_path: "filler", description: "A ripe banana")
+# product = Product.new(name: "banana", price: 0.50, image_path: "filler", description: "A ripe banana")
+
+product2 = Product.new(name: "Tide pods", price: 18, image_path: "filler", description: "Laundry detergent vessel")
+
+product3 = Product.new(name: "Xbox", price: 100, image_path: "filler", description: "home gaming system")
+
+product4 = Product.new(name: "Hair Brush", price: 2, image_path: "filler", description: "Hair detangling tool")
+
+product5 = Product.new(name: "Dog Toy", price: 4, image_path: "filler", description: "durable squeaky toy for dogs")
